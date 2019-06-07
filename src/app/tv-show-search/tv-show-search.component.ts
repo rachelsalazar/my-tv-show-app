42ee4b2e667eec656d7aeb5c25fb2d16a7d5b20e
Rachel Salazar <ret1992@gmail.com>
2019-06-07 12:41:34 -0700

    added validators

--- src/app/tv-show-search/tv-show-search.component.ts
+++ src/app/tv-show-search/tv-show-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { TvShowService } from '../tv-show/tv-show.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tv-show-search',
@@ -8,12 +9,12 @@ import { TvShowService } from '../tv-show/tv-show.service';
   styleUrls: ['./tv-show-search.component.css']
 })
 export class TvShowSearchComponent implements OnInit {
-  search = new FormControl()
+  search = new FormControl('', [Validators.minLength(3)])
 
   constructor(private tvShowService: TvShowService) { }
 
   ngOnInit() {
-    this.search.valueChanges.subscribe((searchValue : string) => {
+    this.search.valueChanges.pipe(debounceTime(1000)).subscribe((searchValue : string) => {
       if (searchValue) {
         const userInput = searchValue.split(' ').map(s => s.trim());
         this.tvShowService.getTvShow(userInput[0]).subscribe(data => console.log(data))
